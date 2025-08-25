46c97b97c84f0fe3909854cece82e260a1d1a50f
= <=>
2025-08-25 17:19:33 +0530

    multer changes

--- config/multer.js
+++ config/multer.js
@@ -1,5 +1,6 @@
-import multer from "multer";
+// import multer from "multer";
 
+// 1
 // const storage = multer.diskStorage({
 //   destination: "uploads",
 //   filename: (req, file, cb) => {
@@ -8,6 +9,30 @@ import multer from "multer";
 // });
 
 // export const upload = multer({ storage });
-const upload = multer({ dest: './public/uploads/' })
 
-export default upload
+// 2
+// const upload = multer({ dest: './public/uploads/' })
+
+// export default upload
+
+// 3
+import multer from "multer";
+import path from "path";
+import fs from "fs";
+
+// Use /tmp directory instead
+const uploadPath = "/uploads";
+
+// Ensure /uploads exists
+fs.mkdirSync(uploadPath, { recursive: true });
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, uploadPath);
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + path.extname(file.originalname));
+  },
+});
+
+export default upload = multer({ storage });
