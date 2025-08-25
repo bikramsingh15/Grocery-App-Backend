5c77c56660eda4b870722b2ae6022d5df27035bc
= <=>
2025-08-25 16:06:54 +0530

    updated .gitignore

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-import express from 'express'
+import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
-import { connectDB } from './config/connectDB.js';
-import userRoutes from './routes/user.routes.js'
-import sellerRoutes from './routes/seller.routes.js'
-import productRoutes from './routes/product.routes.js'
-import cartRoutes from './routes/cart.routes.js'
-import orderRoutes from './routes/order.routes.js'
-import addressRoutes from './routes/address.routes.js'
-import { connectCloudinary } from './config/cloudinary.js';
+import { connectDB } from "./config/connectDB.js";
+import userRoutes from "./routes/user.routes.js";
+import sellerRoutes from "./routes/seller.routes.js";
+import productRoutes from "./routes/product.routes.js";
+import cartRoutes from "./routes/cart.routes.js";
+import orderRoutes from "./routes/order.routes.js";
+import addressRoutes from "./routes/address.routes.js";
+import { connectCloudinary } from "./config/cloudinary.js";
 
 dotenv.config();
 
@@ -18,24 +18,26 @@ const app = express();
 connectDB();
 connectCloudinary();
 
-const allowedOrigins = ['http://localhost:5173']
+const allowedOrigins = ["http://localhost:5173"];
 // middlewares
-app.use(express.json())
-app.use(cors({ origin: allowedOrigins, credentials: true }))
-app.use(cookieParser())
-
+app.use(express.json());
+app.use(cors({ origin: allowedOrigins, credentials: true }));
+app.use(cookieParser());
 
 // API EndPoints
-app.use('/images', express.static('uploads'))
-app.use('/api/user', userRoutes);
-app.use('/api/seller', sellerRoutes);
-app.use('/api/product', productRoutes);
-app.use('/api/cart', cartRoutes);
-app.use('/api/order', orderRoutes);
-app.use('/api/address', addressRoutes);
+app.use("/", (req, res) => {
+  return res.send({ message: "I am Live on Vercel" });
+});
+app.use("/images", express.static("uploads"));
+app.use("/api/user", userRoutes);
+app.use("/api/seller", sellerRoutes);
+app.use("/api/product", productRoutes);
+app.use("/api/cart", cartRoutes);
+app.use("/api/order", orderRoutes);
+app.use("/api/address", addressRoutes);
 
 const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, ()=> {
-    console.log("Server is running");    
-})
+app.listen(PORT, () => {
+  console.log("Server is running");
+});
